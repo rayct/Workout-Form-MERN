7bd8f6c655bc5670d6e1d90d1fbb6eb7ef531b89
rayct <rayct@icloud.com>
2022-07-30 16:23:04 +0100

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
     console.log('connected to database')
     // listen to port
     app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+      console.log('Ray it is working and listening for requests on port', process.env.PORT)
     })
   })
   .catch((err) => {
