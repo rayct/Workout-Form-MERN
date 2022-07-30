cc2309eb6037942fccea8d983018987ebc89bc63
rayct <rayct@icloud.com>
2022-07-30 01:15:11 +0100

    Completed the Backend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, _res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
 // routes
-app.get('/', (_req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
