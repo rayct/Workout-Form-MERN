d73056a67adacf458f83d3aa17ebfba33ba15b42
rayct <rayct@icloud.com>
2022-08-02 01:44:18 +0100

    Backend Baked in and Complete

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -2,7 +2,7 @@ const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
 
 // get all workouts
-const getWorkouts = async (req, res) => {
+const getWorkouts = async (_req, res) => {
   const workouts = await Workout.find({}).sort({createdAt: -1})
 
   res.status(200).json(workouts)
